47b5d6b13c15310f03c7633ebe7e11bea3b7e5e9
willyhsu1106101 <willyhsu1106@gmail.com>
2024-09-10 22:10:10 +0800

    etf

--- etf/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  import { RouterView } from 'vue-router'
-  import './assets/css/bootstrap.min.css'
-  import './assets/css/login.css'
-</script>
-
-<template> 
-  <RouterView />
-</template>
